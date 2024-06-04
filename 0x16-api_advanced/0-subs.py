8776e68f0e38db1b8d3ce9a7c8d3e73d6acd0c49
zezomo <zezouniverse7@gmail.com>
2024-06-04 23:52:58 +0300

    shell 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
     response = requests.get(url, headers=headers)
